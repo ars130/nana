b2ea5e283d327da9a55eede89869b1b856f0713f
ars130 <90564836+ars130@users.noreply.github.com>
2023-09-19 16:20:40 +0300

    Add files via upload

--- turtle.sql
+++ turtle.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 5.2.1deb1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Sep 12, 2023 at 01:09 PM
--- Server version: 5.6.34-log
--- PHP Version: 7.1.5
+-- Хост: localhost:3306
+-- Время создания: Сен 19 2023 г., 16:18
+-- Версия сервера: 10.11.3-MariaDB-1
+-- Версия PHP: 8.2.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,24 +18,34 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `turtle`
+-- База данных: `turtle`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `fraction`
+-- Структура таблицы `fraction`
 --
 
 CREATE TABLE `fraction` (
   `id` int(11) NOT NULL,
   `fraction` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Дамп данных таблицы `fraction`
+--
+
+INSERT INTO `fraction` (`id`, `fraction`) VALUES
+(1, 'mutant'),
+(2, 'mutant'),
+(3, 'mutant'),
+(4, 'mutant');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `personagi`
+-- Структура таблицы `personagi`
 --
 
 CREATE TABLE `personagi` (
@@ -44,12 +53,22 @@ CREATE TABLE `personagi` (
   `name` varchar(50) NOT NULL,
   `age` int(11) NOT NULL,
   `stats` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Дамп данных таблицы `personagi`
+--
+
+INSERT INTO `personagi` (`id`, `name`, `age`, `stats`) VALUES
+(1, 'leonardo', 50, 'kind'),
+(2, 'rafael', 50, 'kind'),
+(3, 'mikelandjelo', 50, 'kind'),
+(4, 'donatello', 50, 'kind');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `race`
+-- Структура таблицы `race`
 --
 
 CREATE TABLE `race` (
@@ -57,24 +76,43 @@ CREATE TABLE `race` (
   `name_race` varchar(50) NOT NULL,
   `origin_world` varchar(50) NOT NULL,
   `peculiarity` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Дамп данных таблицы `race`
+--
+
+INSERT INTO `race` (`id`, `name_race`, `origin_world`, `peculiarity`) VALUES
+(1, 'mutant', 'comics', 'turtle'),
+(2, 'mutant', 'comics', 'turtle'),
+(3, 'mutant', 'comics', 'turtle'),
+(4, 'mutant', 'comics', 'turtle');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Структура таблицы `user`
 --
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `login` varchar(50) NOT NULL,
   `pass` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Дамп данных таблицы `user`
+--
+
+INSERT INTO `user` (`id`, `login`, `pass`) VALUES
+(1, 'user_1', '12345'),
+(2, 'admin', '12345'),
+(3, 'gladmin', '123456789');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `weapon`
+-- Структура таблицы `weapon`
 --
 
 CREATE TABLE `weapon` (
@@ -82,12 +120,21 @@ CREATE TABLE `weapon` (
   `name` varchar(50) NOT NULL,
   `type` varchar(50) NOT NULL,
   `damage_type` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Дамп данных таблицы `weapon`
+--
+
+INSERT INTO `weapon` (`id`, `name`, `type`, `damage_type`) VALUES
+(1, 'katany', 'close combat', 'cutting damage'),
+(2, 'sai', 'close combat', 'cutting damage'),
+(3, 'nunchucks', 'close combat', 'cutting damage');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `world`
+-- Структура таблицы `world`
 --
 
 CREATE TABLE `world` (
@@ -96,86 +143,97 @@ CREATE TABLE `world` (
   `versions` varchar(50) NOT NULL,
   `description` varchar(50) NOT NULL,
   `season_appearance` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Indexes for dumped tables
+-- Дамп данных таблицы `world`
+--
+
+INSERT INTO `world` (`id`, `name_world`, `versions`, `description`, `season_appearance`) VALUES
+(1, 'Earth', '1', 'turtle', '1'),
+(2, 'Earth', '1', 'turtle', '1'),
+(3, 'Earth', '1', 'turtle', '1');
+
+--
+-- Индексы сохранённых таблиц
 --
 
 --
--- Indexes for table `fraction`
+-- Индексы таблицы `fraction`
 --
 ALTER TABLE `fraction`
   ADD KEY `id` (`id`);
 
 --
--- Indexes for table `personagi`
+-- Индексы таблицы `personagi`
 --
 ALTER TABLE `personagi`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `race`
+-- Индексы таблицы `race`
 --
 ALTER TABLE `race`
   ADD KEY `id` (`id`);
 
 --
--- Indexes for table `user`
+-- Индексы таблицы `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `weapon`
+-- Индексы таблицы `weapon`
 --
 ALTER TABLE `weapon`
   ADD KEY `id` (`id`);
 
 --
--- Indexes for table `world`
+-- Индексы таблицы `world`
 --
 ALTER TABLE `world`
   ADD KEY `id` (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT для сохранённых таблиц
 --
 
 --
--- AUTO_INCREMENT for table `personagi`
+-- AUTO_INCREMENT для таблицы `personagi`
 --
 ALTER TABLE `personagi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT для таблицы `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
--- Constraints for dumped tables
+-- Ограничения внешнего ключа сохраненных таблиц
 --
 
 --
--- Constraints for table `fraction`
+-- Ограничения внешнего ключа таблицы `fraction`
 --
 ALTER TABLE `fraction`
   ADD CONSTRAINT `fraction_ibfk_1` FOREIGN KEY (`id`) REFERENCES `personagi` (`id`);
 
 --
--- Constraints for table `race`
+-- Ограничения внешнего ключа таблицы `race`
 --
 ALTER TABLE `race`
   ADD CONSTRAINT `race_ibfk_1` FOREIGN KEY (`id`) REFERENCES `personagi` (`id`);
 
 --
--- Constraints for table `weapon`
+-- Ограничения внешнего ключа таблицы `weapon`
 --
 ALTER TABLE `weapon`
   ADD CONSTRAINT `weapon_ibfk_1` FOREIGN KEY (`id`) REFERENCES `personagi` (`id`);
 
 --
--- Constraints for table `world`
+-- Ограничения внешнего ключа таблицы `world`
 --
 ALTER TABLE `world`
   ADD CONSTRAINT `world_ibfk_1` FOREIGN KEY (`id`) REFERENCES `personagi` (`id`);
